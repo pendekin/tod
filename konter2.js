c0520af4e169aa6ec51c9171d7690b10514540f4
pendekin <80629492+pendekin@users.noreply.github.com>
2022-01-26 06:31:32 +0700

    Create konter2.js

---
+++ konter2.js
@@ -0,0 +1,18 @@
+var counter1 = 10;
+		function countDown2(){
+			if(counter1 >= 0){
+				document.getElementById("timer2").innerHTML = counter;
+			} else {
+				download1();
+				return;
+			}
+			counter1 -=1;
+			var counter3 = setTimeout("countDown2()",2000);
+			return;
+		}
+		function download1(){
+			document.getElementById("link2").innerHTML = "<a class='bt-success' onclick='NewTab(), Go() ' role='button'>Link2</a>";
+ {
+   document.getElementById("timer4").style.display="none";
+   }
+		}
