65ae411b93aa4839b655ebda3dc19ce06abd6f6d
pendekin <80629492+pendekin@users.noreply.github.com>
2022-01-26 06:19:43 +0700

    Create goto.js

---
+++ goto.js
@@ -0,0 +1,18 @@
+if(get('goto')){
+  var goto=get('goto');
+  document.getElementById('goto').style.display = '';
+  }
+  else{
+document.getElementById('goto').style.display = 'none';
+}
+
+function get(property){
+var url=window.location.search;
+  url=url.substring(1).split('&');
+  for(var i=0;i < url.length;i++){
+    var data=url[i].split('=');
+    if(data[0] == property){
+      return data[1];
+    }
+  }
+}
