632b57d94692bc466e307bd3b55f5c417ba51116
pendekin <80629492+pendekin@users.noreply.github.com>
2022-01-31 18:25:54 +0700

    N

--- baru.js
+++ baru.js
@@ -1,3 +1,3 @@
 function NewTab() { 
- window.open("https://apprefaculty.pro/bj3.V_0eP/3EpsvQb/mhVnJ/ZwDI0U0DMmzbIK3WNADfgf3dL/TuQvzwMIjzcB0-OND_gn", "_blank"); 
+ window.open("https://benoopto.com/4/4834082", "_blank"); 
 } 
